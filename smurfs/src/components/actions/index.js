3283ceb0eb413bd7dff7f57fc13a9c9b2739de39
Raudel Flores <rawdell@outlook.com>
2020-02-07 21:31:20 -0600

    finished setting up the postSmurfs action

--- smurfs/src/components/actions/index.js
+++ smurfs/src/components/actions/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { bindActionCreators } from 'redux';
 
 // Actions
 export const FETCH_SMURFS = 'FETCH_SMURFS';
 export const UPDATE_SMURFS = 'UPDATE_SMURFS';
+export const POST_SMURFS = 'POST_SMURFS';
+export const POST_SUCCESS = 'POST_SUCCESS';
 export const ERROR = 'ERROR';
 
 export const fetchSmurfs = () => dispatch => {
@@ -19,4 +22,20 @@ export const fetchSmurfs = () => dispatch => {
               dispatch({ type: ERROR, payload: 'There was an issue retrieving Smurfs'})
             });
     }, 2000)
+}
+
+export const postSmurfs = smurfs => dispatch => {
+    dispatch({ type: POST_SMURFS});
+    setTimeout(() => {
+        axios
+            .post('http://localhost:3333/smurfs', smurfs)
+            .then(res => {
+                console.log('Actions > postSmurfs > Data: ', res);
+                dispatch({ type: POST_SUCCESS, payload: res})
+            })
+            .catch(err => {
+                console.log('Actions > postSmurfs > Error: ', err);
+                dispatch({ type: ERROR, payload: 'There was an issue submitting your Smurf'})
+            })
+    })
 }
