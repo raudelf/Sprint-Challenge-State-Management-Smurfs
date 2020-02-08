6865aafe2261c1b074122932d90c4e6c4ea3e55f
Raudel Flores <rawdell@outlook.com>
2020-02-07 20:48:25 -0600

    finished setting up the Fetching Actions in the smurfReducer

--- smurfs/src/components/reducers/index.js
+++ smurfs/src/components/reducers/index.js
@@ -1,3 +1,6 @@
+// Actions
+import { UPDATE_SMURFS, FETCH_SMURFS, ERROR } from '../actions';
+
 const initialState = {
     smurfs: [],
     isFetching: false,
@@ -6,6 +9,24 @@ const initialState = {
 
 export const smurfReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_SMURFS :
+            return {
+                ...state,
+                smurfs: [],
+                isFetching: true
+            }
+        case UPDATE_SMURFS :
+            return {
+                ...state,
+                smurfs: action.payload,
+                isFetching: false
+            }
+        case ERROR :
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
         default:
             return state;
     };
