c47fc8c099399b64b21b2fc50dc00f802d828494
Raudel Flores <rawdell@outlook.com>
2020-02-07 20:19:24 -0600

    Finished setting up the index.js file

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Reducer
+import { smurfReducer as reducer } from './components/reducers';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById("root"));
