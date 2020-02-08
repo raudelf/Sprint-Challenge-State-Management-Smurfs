171671adf52c2e1e394846e4a9ff7b3c0af53e5d
Raudel Flores <rawdell@outlook.com>
2020-02-07 21:45:52 -0600

    created case actions for posting

--- smurfs/src/components/reducers/index.js
+++ smurfs/src/components/reducers/index.js
@@ -1,9 +1,10 @@
 // Actions
-import { UPDATE_SMURFS, FETCH_SMURFS, ERROR } from '../actions';
+import { UPDATE_SMURFS, FETCH_SMURFS, POST_SMURFS, POST_SUCCESS, ERROR } from '../actions';
 
 const initialState = {
     smurfs: [],
     isFetching: false,
+    isPosting: false,
     error: ''
 }
 
@@ -21,6 +22,17 @@ export const smurfReducer = (state = initialState, action) => {
                 ...state,
                 smurfs: action.payload,
                 isFetching: false
+            }
+        case POST_SMURFS :
+            return {
+                ...state,
+                isPosting: true,
+                error: ''
+            }
+        case POST_SUCCESS :
+            return {
+                ...state,
+                smurfs: [...state.smurfs, action.payload]
             }
         case ERROR :
             return {
